68b6e09f9b97d745509ec8ad6f770377560d21ae
Keith <peralta.keit@outlook.com>
2021-04-05 04:18:21 +1200

    add restsharp and expense model

---
+++ TechnicalTest/Models/Expense.cs
@@ -0,0 +1,17 @@
+﻿using RestSharp.Deserializers;
+
+namespace TechnicalTest.Models
+{
+    [DeserializeAs(Name = "expense")]
+    public class Expense
+    {
+        [DeserializeAs(Name = "cost_centre")]
+        public string CostCentre { get; set; }
+
+        [DeserializeAs(Name = "total")]
+        public decimal? Total { get; set; }
+
+        [DeserializeAs(Name = "payment_method")]
+        public string PaymentMethod { get; set; }
+    }
+}
