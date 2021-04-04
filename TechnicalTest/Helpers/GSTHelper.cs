d84723ab09194a3133c2bc79225fe0258a641743
Keith <peralta.keit@outlook.com>
2021-04-05 04:24:23 +1200

    add gst calculator helper method

---
+++ TechnicalTest/Helpers/GSTHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace TechnicalTest.Helpers
+{
+    public static class GSTHelper
+    {
+        public static decimal GSTCalculator(decimal gstInclusiveAmount)
+        {
+            decimal _gstAmount = (gstInclusiveAmount * 3) / 23;
+            return _gstAmount;
+        }
+    }
+}
